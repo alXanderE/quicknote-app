007c7438e879b6a5ae5278e0bdca6fb8a7df37cd
Alexander Eng <alexbird300@gmail.com>
2026-02-24 22:04:06 -0800

    added ui and stuff

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+load_dotenv()
+
+app = Flask(__name__)
+
+print("MONGO URI:", os.getenv("MONGO_URI"))
+# MongoDB connection
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["quicknote"]
+notes_collection = db["notes"]
+
+@app.route("/")
+def index():
+    notes = notes_collection.find().sort("created_at", -1)
+    return render_template("index.html", notes=notes)
+
+@app.route("/add", methods=["POST"])
+def add_note():
+    text = request.form.get("text")
+
+    if text:
+        notes_collection.insert_one({
+            "text": text,
+            "created_at": datetime.utcnow()
+        })
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
